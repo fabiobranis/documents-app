371bdb2ea52bb0836cb82e1a4bde92519a04b969
fabiobranis <fabriobranis@gmail.com>
2019-03-18 21:48:39 -0300

    Validation on delete

--- express-server/services/documents/Delete.js
+++ express-server/services/documents/Delete.js
@@ -4,10 +4,10 @@ export default async (request, response) => {
     try {
         Document.findById(request.params.id).exec((error, document) => {
             if (!document) {
-                return response.json({message: 'No document to delete'})
+                return response.json({ message: 'No document to delete' });
             }
             document.remove(() => {
-                response.status(200).json({message: 'Document deleted'});
+                response.status(200).json({ message: 'Document deleted' });
             });
         });
     } catch (error) {
